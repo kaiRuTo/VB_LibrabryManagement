99e60127d6d56c01013e97ba2a6cd7e6656411c7
mrGos <hoangkhang0235@gmail.com>
2018-05-31 22:01:30 +0700

    join
    ada

--- LibraryManagement/LibraryManagement/frmMainQLTVGUI.Designer.vb
+++ LibraryManagement/LibraryManagement/frmMainQLTVGUI.Designer.vb
@@ -35,6 +35,9 @@ Partial Class frmMain
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 98da15f... again
         Me.FileToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
         Me.QuyĐịnhToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.HệThốngTácGiảToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
@@ -44,12 +47,15 @@ Partial Class frmMain
         Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
         Me.QuanLySachToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
 >>>>>>> 988666cf67b8faffc6b2bf1a1817545712ec10f1
+<<<<<<< HEAD
 =======
 >>>>>>> parent of 988666c... Add frmSachGUI (unfinished)
 =======
 >>>>>>> parent of 988666c... Add frmSachGUI (unfinished)
 =======
 >>>>>>> parent of 342025a... them tg,nxb
+=======
+>>>>>>> parent of 98da15f... again
         Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -63,11 +69,15 @@ Partial Class frmMain
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 98da15f... again
         Me.MenuStrip1.Size = New System.Drawing.Size(451, 28)
 =======
         Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(4, 2, 0, 2)
         Me.MenuStrip1.Size = New System.Drawing.Size(359, 24)
 >>>>>>> 988666cf67b8faffc6b2bf1a1817545712ec10f1
+<<<<<<< HEAD
 =======
         Me.MenuStrip1.Size = New System.Drawing.Size(282, 28)
 >>>>>>> parent of 988666c... Add frmSachGUI (unfinished)
@@ -77,6 +87,8 @@ Partial Class frmMain
 =======
         Me.MenuStrip1.Size = New System.Drawing.Size(282, 28)
 >>>>>>> parent of 342025a... them tg,nxb
+=======
+>>>>>>> parent of 98da15f... again
         Me.MenuStrip1.TabIndex = 1
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -84,54 +96,57 @@ Partial Class frmMain
         '
         Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.QuảnLýĐọcGiảToolStripMenuItem, Me.ToolStripMenuItem1, Me.QuảnLyLoạiĐọcGiảToolStripMenuItem, Me.ToolStripMenuItem2})
         Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
-        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(137, 24)
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(111, 20)
         Me.FileToolStripMenuItem.Text = "Hệ thống đọc giả"
         '
         'QuảnLýĐọcGiảToolStripMenuItem
         '
         Me.QuảnLýĐọcGiảToolStripMenuItem.Name = "QuảnLýĐọcGiảToolStripMenuItem"
-        Me.QuảnLýĐọcGiảToolStripMenuItem.Size = New System.Drawing.Size(217, 26)
+        Me.QuảnLýĐọcGiảToolStripMenuItem.Size = New System.Drawing.Size(179, 22)
         Me.QuảnLýĐọcGiảToolStripMenuItem.Text = "Quản lý loại đọc giả"
         '
         'ToolStripMenuItem1
         '
         Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
-        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(214, 6)
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(176, 6)
         '
         'QuảnLyLoạiĐọcGiảToolStripMenuItem
         '
         Me.QuảnLyLoạiĐọcGiảToolStripMenuItem.Name = "QuảnLyLoạiĐọcGiảToolStripMenuItem"
-        Me.QuảnLyLoạiĐọcGiảToolStripMenuItem.Size = New System.Drawing.Size(217, 26)
+        Me.QuảnLyLoạiĐọcGiảToolStripMenuItem.Size = New System.Drawing.Size(179, 22)
         Me.QuảnLyLoạiĐọcGiảToolStripMenuItem.Text = "Quản ly đọc giả"
         '
         'ToolStripMenuItem2
         '
         Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
-        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(214, 6)
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(176, 6)
         '
         'HệThốngSáchToolStripMenuItem
         '
-        Me.HệThốngSáchToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ThêmLoạiSáchToolStripMenuItem, Me.QuảnLýLoạiSáchToolStripMenuItem})
+        Me.HệThốngSáchToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ThêmLoạiSáchToolStripMenuItem, Me.QuảnLýLoạiSáchToolStripMenuItem, Me.ToolStripSeparator1, Me.QuanLySachToolStripMenuItem})
         Me.HệThốngSáchToolStripMenuItem.Name = "HệThốngSáchToolStripMenuItem"
-        Me.HệThốngSáchToolStripMenuItem.Size = New System.Drawing.Size(118, 24)
+        Me.HệThốngSáchToolStripMenuItem.Size = New System.Drawing.Size(97, 20)
         Me.HệThốngSáchToolStripMenuItem.Text = "Hệ thống Sách"
         '
         'ThêmLoạiSáchToolStripMenuItem
         '
         Me.ThêmLoạiSáchToolStripMenuItem.Name = "ThêmLoạiSáchToolStripMenuItem"
-        Me.ThêmLoạiSáchToolStripMenuItem.Size = New System.Drawing.Size(196, 26)
+        Me.ThêmLoạiSáchToolStripMenuItem.Size = New System.Drawing.Size(164, 22)
         Me.ThêmLoạiSáchToolStripMenuItem.Text = "Thêm Loại Sách"
         '
         'QuảnLýLoạiSáchToolStripMenuItem
         '
         Me.QuảnLýLoạiSáchToolStripMenuItem.Name = "QuảnLýLoạiSáchToolStripMenuItem"
-        Me.QuảnLýLoạiSáchToolStripMenuItem.Size = New System.Drawing.Size(196, 26)
+        Me.QuảnLýLoạiSáchToolStripMenuItem.Size = New System.Drawing.Size(164, 22)
         Me.QuảnLýLoạiSáchToolStripMenuItem.Text = "Quản lý loại sách"
         '
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 98da15f... again
         'FileToolStripMenuItem1
         '
         Me.FileToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.QuyĐịnhToolStripMenuItem})
@@ -176,24 +191,31 @@ Partial Class frmMain
         Me.QuanLySachToolStripMenuItem.Text = "Quản lý sách"
 >>>>>>> 988666cf67b8faffc6b2bf1a1817545712ec10f1
         '
+<<<<<<< HEAD
 =======
 >>>>>>> parent of 988666c... Add frmSachGUI (unfinished)
 =======
 >>>>>>> parent of 988666c... Add frmSachGUI (unfinished)
 =======
 >>>>>>> parent of 342025a... them tg,nxb
+=======
+>>>>>>> parent of 98da15f... again
         'frmMain
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 98da15f... again
         Me.ClientSize = New System.Drawing.Size(451, 253)
 =======
         Me.ClientSize = New System.Drawing.Size(359, 217)
 >>>>>>> 988666cf67b8faffc6b2bf1a1817545712ec10f1
+<<<<<<< HEAD
 =======
         Me.ClientSize = New System.Drawing.Size(282, 253)
 >>>>>>> parent of 988666c... Add frmSachGUI (unfinished)
@@ -203,9 +225,12 @@ Partial Class frmMain
 =======
         Me.ClientSize = New System.Drawing.Size(282, 253)
 >>>>>>> parent of 342025a... them tg,nxb
+=======
+>>>>>>> parent of 98da15f... again
         Me.Controls.Add(Me.MenuStrip1)
         Me.IsMdiContainer = True
         Me.MainMenuStrip = Me.MenuStrip1
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "frmMain"
         Me.Text = "Quản Lý Thư Viện"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
@@ -229,6 +254,9 @@ Partial Class frmMain
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 98da15f... again
     Friend WithEvents FileToolStripMenuItem1 As ToolStripMenuItem
     Friend WithEvents QuyĐịnhToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents HệThốngTácGiảToolStripMenuItem As ToolStripMenuItem
@@ -238,10 +266,13 @@ Partial Class frmMain
     Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
     Friend WithEvents QuanLySachToolStripMenuItem As ToolStripMenuItem
 >>>>>>> 988666cf67b8faffc6b2bf1a1817545712ec10f1
+<<<<<<< HEAD
 =======
 >>>>>>> parent of 988666c... Add frmSachGUI (unfinished)
 =======
 >>>>>>> parent of 988666c... Add frmSachGUI (unfinished)
 =======
 >>>>>>> parent of 342025a... them tg,nxb
+=======
+>>>>>>> parent of 98da15f... again
 End Class
---
+++ LibraryManagement/LibraryManagement/frmSachGUI.Designer.vb
@@ -0,0 +1,263 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSachGUI
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.textBoxSoLuongConLai = New System.Windows.Forms.TextBox()
+        Me.labelConLai = New System.Windows.Forms.Label()
+        Me.textBoxSoLuongSach = New System.Windows.Forms.TextBox()
+        Me.labelSoLuongSach = New System.Windows.Forms.Label()
+        Me.textBoxTriGia = New System.Windows.Forms.TextBox()
+        Me.labelTriGia = New System.Windows.Forms.Label()
+        Me.labelNgayNhap = New System.Windows.Forms.Label()
+        Me.dtpNgayNhap = New System.Windows.Forms.DateTimePicker()
+        Me.comboBoxNhaXuatBan = New System.Windows.Forms.ComboBox()
+        Me.labelNhaXuatBan = New System.Windows.Forms.Label()
+        Me.textBoxTenTacGia = New System.Windows.Forms.TextBox()
+        Me.labelTenTacGia = New System.Windows.Forms.Label()
+        Me.comboBoxLoaiSach = New System.Windows.Forms.ComboBox()
+        Me.labelLoaiSach = New System.Windows.Forms.Label()
+        Me.textBoxTenSach = New System.Windows.Forms.TextBox()
+        Me.labelTenSach = New System.Windows.Forms.Label()
+        Me.textBoxMaSach = New System.Windows.Forms.TextBox()
+        Me.labelMaSach = New System.Windows.Forms.Label()
+        Me.buttonThemSach = New System.Windows.Forms.Button()
+        Me.comboBoxTacGia = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'textBoxSoLuongConLai
+        '
+        Me.textBoxSoLuongConLai.Location = New System.Drawing.Point(114, 352)
+        Me.textBoxSoLuongConLai.Name = "textBoxSoLuongConLai"
+        Me.textBoxSoLuongConLai.Size = New System.Drawing.Size(173, 20)
+        Me.textBoxSoLuongConLai.TabIndex = 36
+        '
+        'labelConLai
+        '
+        Me.labelConLai.AutoSize = True
+        Me.labelConLai.Location = New System.Drawing.Point(37, 354)
+        Me.labelConLai.Name = "labelConLai"
+        Me.labelConLai.Size = New System.Drawing.Size(43, 13)
+        Me.labelConLai.TabIndex = 35
+        Me.labelConLai.Text = "Còn Lại"
+        '
+        'textBoxSoLuongSach
+        '
+        Me.textBoxSoLuongSach.Location = New System.Drawing.Point(115, 317)
+        Me.textBoxSoLuongSach.Name = "textBoxSoLuongSach"
+        Me.textBoxSoLuongSach.Size = New System.Drawing.Size(173, 20)
+        Me.textBoxSoLuongSach.TabIndex = 34
+        '
+        'labelSoLuongSach
+        '
+        Me.labelSoLuongSach.AutoSize = True
+        Me.labelSoLuongSach.Location = New System.Drawing.Point(38, 319)
+        Me.labelSoLuongSach.Name = "labelSoLuongSach"
+        Me.labelSoLuongSach.Size = New System.Drawing.Size(49, 13)
+        Me.labelSoLuongSach.TabIndex = 33
+        Me.labelSoLuongSach.Text = "Số lượng"
+        '
+        'textBoxTriGia
+        '
+        Me.textBoxTriGia.Location = New System.Drawing.Point(116, 282)
+        Me.textBoxTriGia.Name = "textBoxTriGia"
+        Me.textBoxTriGia.Size = New System.Drawing.Size(173, 20)
+        Me.textBoxTriGia.TabIndex = 32
+        '
+        'labelTriGia
+        '
+        Me.labelTriGia.AutoSize = True
+        Me.labelTriGia.Location = New System.Drawing.Point(39, 284)
+        Me.labelTriGia.Name = "labelTriGia"
+        Me.labelTriGia.Size = New System.Drawing.Size(36, 13)
+        Me.labelTriGia.TabIndex = 31
+        Me.labelTriGia.Text = "Trị giá"
+        '
+        'labelNgayNhap
+        '
+        Me.labelNgayNhap.AutoSize = True
+        Me.labelNgayNhap.Location = New System.Drawing.Point(38, 249)
+        Me.labelNgayNhap.Name = "labelNgayNhap"
+        Me.labelNgayNhap.Size = New System.Drawing.Size(59, 13)
+        Me.labelNgayNhap.TabIndex = 30
+        Me.labelNgayNhap.Text = "Ngày nhập"
+        '
+        'dtpNgayNhap
+        '
+        Me.dtpNgayNhap.Location = New System.Drawing.Point(116, 247)
+        Me.dtpNgayNhap.Name = "dtpNgayNhap"
+        Me.dtpNgayNhap.Size = New System.Drawing.Size(200, 20)
+        Me.dtpNgayNhap.TabIndex = 29
+        '
+        'comboBoxNhaXuatBan
+        '
+        Me.comboBoxNhaXuatBan.FormattingEnabled = True
+        Me.comboBoxNhaXuatBan.Location = New System.Drawing.Point(115, 211)
+        Me.comboBoxNhaXuatBan.Name = "comboBoxNhaXuatBan"
+        Me.comboBoxNhaXuatBan.Size = New System.Drawing.Size(133, 21)
+        Me.comboBoxNhaXuatBan.TabIndex = 28
+        '
+        'labelNhaXuatBan
+        '
+        Me.labelNhaXuatBan.AutoSize = True
+        Me.labelNhaXuatBan.Location = New System.Drawing.Point(38, 214)
+        Me.labelNhaXuatBan.Name = "labelNhaXuatBan"
+        Me.labelNhaXuatBan.Size = New System.Drawing.Size(71, 13)
+        Me.labelNhaXuatBan.TabIndex = 27
+        Me.labelNhaXuatBan.Text = "Nhà xuất bản"
+        '
+        'textBoxTenTacGia
+        '
+        Me.textBoxTenTacGia.Location = New System.Drawing.Point(115, 144)
+        Me.textBoxTenTacGia.Name = "textBoxTenTacGia"
+        Me.textBoxTenTacGia.Size = New System.Drawing.Size(201, 20)
+        Me.textBoxTenTacGia.TabIndex = 26
+        '
+        'labelTenTacGia
+        '
+        Me.labelTenTacGia.AutoSize = True
+        Me.labelTenTacGia.Location = New System.Drawing.Point(39, 146)
+        Me.labelTenTacGia.Name = "labelTenTacGia"
+        Me.labelTenTacGia.Size = New System.Drawing.Size(61, 13)
+        Me.labelTenTacGia.TabIndex = 25
+        Me.labelTenTacGia.Text = "Tên tác giả"
+        '
+        'comboBoxLoaiSach
+        '
+        Me.comboBoxLoaiSach.FormattingEnabled = True
+        Me.comboBoxLoaiSach.Location = New System.Drawing.Point(114, 106)
+        Me.comboBoxLoaiSach.Name = "comboBoxLoaiSach"
+        Me.comboBoxLoaiSach.Size = New System.Drawing.Size(133, 21)
+        Me.comboBoxLoaiSach.TabIndex = 24
+        '
+        'labelLoaiSach
+        '
+        Me.labelLoaiSach.AutoSize = True
+        Me.labelLoaiSach.Location = New System.Drawing.Point(38, 109)
+        Me.labelLoaiSach.Name = "labelLoaiSach"
+        Me.labelLoaiSach.Size = New System.Drawing.Size(53, 13)
+        Me.labelLoaiSach.TabIndex = 23
+        Me.labelLoaiSach.Text = "Loại sách"
+        '
+        'textBoxTenSach
+        '
+        Me.textBoxTenSach.Location = New System.Drawing.Point(114, 69)
+        Me.textBoxTenSach.Name = "textBoxTenSach"
+        Me.textBoxTenSach.Size = New System.Drawing.Size(174, 20)
+        Me.textBoxTenSach.TabIndex = 22
+        '
+        'labelTenSach
+        '
+        Me.labelTenSach.AutoSize = True
+        Me.labelTenSach.Location = New System.Drawing.Point(38, 72)
+        Me.labelTenSach.Name = "labelTenSach"
+        Me.labelTenSach.Size = New System.Drawing.Size(52, 13)
+        Me.labelTenSach.TabIndex = 21
+        Me.labelTenSach.Text = "Tên sách"
+        '
+        'textBoxMaSach
+        '
+        Me.textBoxMaSach.Location = New System.Drawing.Point(114, 32)
+        Me.textBoxMaSach.Name = "textBoxMaSach"
+        Me.textBoxMaSach.ReadOnly = True
+        Me.textBoxMaSach.Size = New System.Drawing.Size(133, 20)
+        Me.textBoxMaSach.TabIndex = 20
+        '
+        'labelMaSach
+        '
+        Me.labelMaSach.AutoSize = True
+        Me.labelMaSach.Location = New System.Drawing.Point(38, 35)
+        Me.labelMaSach.Name = "labelMaSach"
+        Me.labelMaSach.Size = New System.Drawing.Size(48, 13)
+        Me.labelMaSach.TabIndex = 19
+        Me.labelMaSach.Text = "Mã sách"
+        '
+        'buttonThemSach
+        '
+        Me.buttonThemSach.Location = New System.Drawing.Point(224, 392)
+        Me.buttonThemSach.Name = "buttonThemSach"
+        Me.buttonThemSach.Size = New System.Drawing.Size(92, 32)
+        Me.buttonThemSach.TabIndex = 37
+        Me.buttonThemSach.Text = "Thêm"
+        Me.buttonThemSach.UseVisualStyleBackColor = True
+        '
+        'comboBoxTacGia
+        '
+        Me.comboBoxTacGia.FormattingEnabled = True
+        Me.comboBoxTacGia.Location = New System.Drawing.Point(195, 170)
+        Me.comboBoxTacGia.Name = "comboBoxTacGia"
+        Me.comboBoxTacGia.Size = New System.Drawing.Size(121, 21)
+        Me.comboBoxTacGia.TabIndex = 38
+        '
+        'frmSachGUI
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(362, 449)
+        Me.Controls.Add(Me.comboBoxTacGia)
+        Me.Controls.Add(Me.buttonThemSach)
+        Me.Controls.Add(Me.textBoxSoLuongConLai)
+        Me.Controls.Add(Me.labelConLai)
+        Me.Controls.Add(Me.textBoxSoLuongSach)
+        Me.Controls.Add(Me.labelSoLuongSach)
+        Me.Controls.Add(Me.textBoxTriGia)
+        Me.Controls.Add(Me.labelTriGia)
+        Me.Controls.Add(Me.labelNgayNhap)
+        Me.Controls.Add(Me.dtpNgayNhap)
+        Me.Controls.Add(Me.comboBoxNhaXuatBan)
+        Me.Controls.Add(Me.labelNhaXuatBan)
+        Me.Controls.Add(Me.textBoxTenTacGia)
+        Me.Controls.Add(Me.labelTenTacGia)
+        Me.Controls.Add(Me.comboBoxLoaiSach)
+        Me.Controls.Add(Me.labelLoaiSach)
+        Me.Controls.Add(Me.textBoxTenSach)
+        Me.Controls.Add(Me.labelTenSach)
+        Me.Controls.Add(Me.textBoxMaSach)
+        Me.Controls.Add(Me.labelMaSach)
+        Me.Name = "frmSachGUI"
+        Me.Text = "Thêm sách"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents textBoxSoLuongConLai As TextBox
+    Friend WithEvents labelConLai As Label
+    Friend WithEvents textBoxSoLuongSach As TextBox
+    Friend WithEvents labelSoLuongSach As Label
+    Friend WithEvents textBoxTriGia As TextBox
+    Friend WithEvents labelTriGia As Label
+    Friend WithEvents labelNgayNhap As Label
+    Friend WithEvents dtpNgayNhap As DateTimePicker
+    Friend WithEvents comboBoxNhaXuatBan As ComboBox
+    Friend WithEvents labelNhaXuatBan As Label
+    Friend WithEvents textBoxTenTacGia As TextBox
+    Friend WithEvents labelTenTacGia As Label
+    Friend WithEvents comboBoxLoaiSach As ComboBox
+    Friend WithEvents labelLoaiSach As Label
+    Friend WithEvents textBoxTenSach As TextBox
+    Friend WithEvents labelTenSach As Label
+    Friend WithEvents textBoxMaSach As TextBox
+    Friend WithEvents labelMaSach As Label
+    Friend WithEvents buttonThemSach As Button
+    Friend WithEvents comboBoxTacGia As ComboBox
+End Class
